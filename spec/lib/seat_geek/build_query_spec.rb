f7544b14b79441e524eb50ed19dfba4620339556
John Moon <john.moon@livingsocial.com>
2015-12-21 21:50:38 -0500

    Add BuildQuery spec

---
+++ spec/lib/seat_geek/build_query_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe SeatGeek::BuildQuery do
+  describe '#build' do
+    it 'return month_of_the_year url' do
+      expect(1).to be_equal(1)
+    end
+    it 'return state url' do
+      skip
+      expect(1).to be_equal(1)
+    end
+    it 'return attendee_count url' do
+      skip
+      expect(1).to be_equal(1)
+    end
+    it 'return event_type url' do
+      skip
+      expect(1).to be_equal(1)
+    end
+    it 'return base url' do
+      skip
+      expect(1).to be_equal(1)
+    end
+  end
+end
